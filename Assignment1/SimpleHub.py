cab47e40f6fa61c842e500e82e6369eb502817c2
Manish Reddy <manishreddy1993@gmail.com>
2015-10-27 13:14:27 -0400

    Python file

---
+++ Assignment1/SimpleHub.py
@@ -0,0 +1,17 @@
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+from pox.lib.util import dpidToStr
+
+log = core.getLogger()
+
+
+def _handle_ConnectionUp (event):
+  msg = of.ofp_flow_mod()
+  msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
+  event.connection.send(msg)
+  log.info("Hubifying %s", dpidToStr(event.dpid))
+
+def launch ():
+  core.openflow.addListenerByName("ConnectionUp", _handle_ConnectionUp)
+
+  log.info("Hub running.")
