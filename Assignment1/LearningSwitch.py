3a89ac48833c9e19f2d6c73f5a50f7bb55db9af0
manishreddy1993 <manishreddy1993@users.noreply.github.com>
2015-10-27 14:28:25 -0400

    Update LearningSwitch.py

--- Assignment1/LearningSwitch.py
+++ Assignment1/LearningSwitch.py
@@ -17,41 +17,6 @@ log = core.getLogger()
 _flood_delay = 0
 
 class LearningSwitch (object):
-  """
-  The learning switch "brain" associated with a single OpenFlow switch.
-  When we see a packet, we'd like to output it on a port which will
-  eventually lead to the destination.  To accomplish this, we build a
-  table that maps addresses to ports.
-  We populate the table by observing traffic.  When we see a packet
-  from some source coming from some port, we know that source is out
-  that port.
-  When we want to forward traffic, we look up the desintation in our
-  table.  If we don't know the port, we simply send the message out
-  all ports except the one it came in on.  (In the presence of loops,
-  this is bad!).
-  In short, our algorithm looks like this:
-  For each packet from the switch:
-  1) Use source address and switch port to update address/port table
-  2) Is transparent = False and either Ethertype is LLDP or the packet's
-     destination address is a Bridge Filtered address?
-     Yes:
-        2a) Drop packet -- don't forward link-local traffic (LLDP, 802.1x)
-            DONE
-  3) Is destination multicast?
-     Yes:
-        3a) Flood the packet
-            DONE
-  4) Port for destination address in our address/port table?
-     No:
-        4a) Flood the packet
-            DONE
-  5) Is output port the same as input port?
-     Yes:
-        5a) Drop packet and similar ones for a while
-  6) Install flow table entry in the switch so that this
-     flow goes out the appopriate port
-     6a) Send the packet out appropriate port
-  """
   def __init__ (self, connection, transparent):
     # Switch we'll be adding L2 learning switch capabilities to
     self.connection = connection
